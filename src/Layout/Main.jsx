af6092498912bb06ab7150450639676b9d4f5249
sakibmahbub <arafatsakib48@gmail.com>
2023-05-13 01:11:05 +0600

    created navbar and footer

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,5 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Shared/Footer/Footer";
+import Navbar from "../Pages/Shared/Navbar/Navbar";
+
 const Main = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 };
 
 export default Main;
