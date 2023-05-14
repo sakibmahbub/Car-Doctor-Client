f3a2fe12bcb4f7ae5e7616d28da8ba70eedb27ad
sakibmahbub <arafatsakib48@gmail.com>
2023-05-14 23:52:28 +0600

    created checkout form

--- src/Pages/Checkout/Checkout.jsx
+++ src/Pages/Checkout/Checkout.jsx
@@ -4,8 +4,61 @@ const Checkout = () => {
   const service = useLoaderData();
   const { _id, title } = service;
   return (
-    <div>
-      <h2>Checkout for: {title}</h2>
+    <div className="mt-10">
+      <h2 className="text-center text-3xl font-bold">Checkout for: {title}</h2>
+      <div className="card-body">
+        <form>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">First Name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="first name"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Last Name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="last name"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+              />
+            </div>
+          </div>
+          <div className="form-control mt-6">
+            <input
+              type="submit"
+              value="Checkout"
+              className="btn btn-block bg-[#FF3811]"
+            />
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
