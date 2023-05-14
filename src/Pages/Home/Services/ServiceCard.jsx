a460044990350b7ef3924e9f8f043c7b1c291b32
sakibmahbub <arafatsakib48@gmail.com>
2023-05-14 22:21:58 +0600

    loaded services data from server

--- src/Pages/Home/Services/ServiceCard.jsx
+++ src/Pages/Home/Services/ServiceCard.jsx
@@ -1,6 +1,7 @@
 import { FaArrowRight } from "react-icons/fa";
+import { Link } from "react-router-dom";
 const ServiceCard = ({ service }) => {
-  const { title, img, price } = service;
+  const { _id, title, img, price } = service;
   return (
     <div className="card w-96 bg-base-100 shadow-lg">
       <figure className="px-5 pt-5">
@@ -10,9 +11,11 @@ const ServiceCard = ({ service }) => {
         <h2 className="card-title">{title}</h2>
         <div className="flex justify-between mt-2">
           <p className="font-bold text-[#FF3811]">Price: ${price}</p>
-          <button className="cursor-pointer hover:text-[#FF3811]">
-            <FaArrowRight />
-          </button>
+          <Link to={`/checkout/${_id}`}>
+            <button className="cursor-pointer hover:text-[#FF3811]">
+              <FaArrowRight />
+            </button>
+          </Link>
         </div>
       </div>
     </div>
